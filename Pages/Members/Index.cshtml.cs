f9c309a33248561722208c2ff8bc813e7561cf3a
Teona Raluca Artene <teonaartene@yahoo.com>
2024-11-07 16:08:01 +0200

    lab5

---
+++ Pages/Members/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Artene_Teona_Raluca_Lab2.Data;
+using Artene_Teona_Raluca_Lab2.Models;
+
+namespace Artene_Teona_Raluca_Lab2.Pages.Members
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Artene_Teona_Raluca_Lab2.Data.Artene_Teona_Raluca_Lab2Context _context;
+
+        public IndexModel(Artene_Teona_Raluca_Lab2.Data.Artene_Teona_Raluca_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Member> Member { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Member = await _context.Member.ToListAsync();
+        }
+    }
+}
